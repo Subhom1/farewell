1090ea4f1ddd0c13c75c5298c1b493b297271a24
Subhom Kundu <subhomkundu@gmail.com>
2020-03-06 01:57:07 +0530

    Added Files via upload
    Files Added

---
+++ js/index.js
@@ -0,0 +1,8 @@
+showAlert = () => {
+
+    alert("Here's your favorite alert!");
+    alert("Have a great future ahead!");
+    alert("Now go to console");
+    console.log("We all miss you <3  ^_^ Pritam da");
+}
+
